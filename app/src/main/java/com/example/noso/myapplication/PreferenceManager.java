a4ccf8775ab44a84db2ac8f4a1b08406b5347592
AbdelRahman Tarek <abdelrahman.tare.mahdy@gmail.com>
2018-04-11 20:45:17 +0200

    Gradle upgrade fix & always logged in x-auth retrieval

--- app/src/main/java/com/example/noso/myapplication/PreferenceManager.java
+++ app/src/main/java/com/example/noso/myapplication/PreferenceManager.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import java.util.HashMap;
 
@@ -42,8 +43,8 @@ public class PreferenceManager extends AppCompatActivity {
         xAuthToken = xAuth;
         editor.commit();
     }
-    public String returnxAuth()
-    {
+
+    public String returnxAuth() {
         return xAuthToken;
     }
 
@@ -65,6 +66,8 @@ public class PreferenceManager extends AppCompatActivity {
     }
 
     public boolean isLoggedIn() {
+        xAuthToken = pref.getString(KEY_X_AUTH, null);
+        Log.d("O-messenger", "X-auth token: " + xAuthToken);
         return pref.getBoolean(IS_LOGIN, false);
     }
 
